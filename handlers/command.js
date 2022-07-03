72ad878a74452b986b41c001a19a5266d3300b95
Mat0wa3 <67017891+Mat0wa3@users.noreply.github.com>
2022-07-03 18:23:38 -0500

    Create command.js

---
+++ handlers/command.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+module.exports = (client) => {
+    try {
+        let comandos = 0;
+        fs.readdirSync("./comandos/").forEach((carpeta) => {
+            const commands = fs.readdirSync(`./comandos/${carpeta}`).filter((archivo) => archivo.endsWith(".js"));
+            for (let archivo of commands){
+                let comando = require(`../comandos/${carpeta}/${archivo}`);
+                if(comando.name) {
+                    client.commands.set(comando.name, comando);
+                    comandos++
+                } else {
+                    console.log(`COMANDO [/${carpeta}/${archivo}]`, `error => el comando no está configurado`.brightRed)
+                    continue;
+                }
+                if(comando.aliases && Array.isArray(comando.aliases)) comando.aliases.forEach((alias) => client.aliases.set(alias, comando.name));
+            }
+        });
+    } catch(e){
+        console.log(e)
+    }
+}
+
+/*
+* 🟢 Handler hecho por Mateo#9100
+*/
